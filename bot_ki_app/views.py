b460efcbc0341cd88ea5f9f6694e0345233f20df
Rizal Diantoro <iniguerizal@gmail.com>
2018-03-04 11:42:18 +0700

    revert

--- bot_ki_app/views.py
+++ bot_ki_app/views.py
@@ -26,7 +26,7 @@ def callback(request):
             return HttpResponseBadRequest()
 
         for event in events:
-            print (event.source.type)
+            print (event.source.user_id)
 
             # profile = line_bot_api.get_profile(event.source.userId)
             # reply_message_txt = "Halo " + profile.display_name
